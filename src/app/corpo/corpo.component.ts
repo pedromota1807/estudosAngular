72b0ab1b7420837dd7d7defaf3b07d5138abb2db
pedromota1807 <pedrobmotta@hotmail.com>
2021-08-11 21:54:17 -0300

    Criando objetos com typescript

--- src/app/corpo/corpo.component.ts
+++ src/app/corpo/corpo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Cliente } from '../model/Cliente';
 
 @Component({
   selector: 'app-corpo',
@@ -8,13 +9,28 @@ import { Router } from '@angular/router';
 })
 export class CorpoComponent implements OnInit {
 
-  constructor(private nav: Router) { }
+  public cliente: Cliente;
+
+  constructor(private nav: Router) {
+    this.cliente = new Cliente();
+
+    this.cliente.id = 9876;
+    this.cliente.nome = "Pedro";
+    this.cliente.email = "pedro@pedro.com";
+    this.cliente.endereco = "Rua das lagrimas";
+
+    console.log(this.cliente);
+   }
 
   ngOnInit(): void {
   }
 
   navegar(){
     this.nav.navigate(["/sobre"])
+  }
+
+  alterar(){
+    this.cliente.nome = "Mota, Pedro";
   }
 
 }
---
+++ src/app/model/Cliente.ts
@@ -0,0 +1,11 @@
+export class Cliente{
+
+  public id!: number;
+  public nome!: string;
+  public endereco!: string;
+  public email!: string;
+
+  public constructor(){
+    console.log("estou no construtor");
+  }
+}
